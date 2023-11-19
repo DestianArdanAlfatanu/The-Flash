33a41e0ccab92fe101278ace7b5034ff10951e88
SwiftMaxz <gpnaxs283@gmail.com>
2023-11-19 19:52:56 +0700

    minor update

--- db.sql
+++ db.sql
@@ -3,18 +3,21 @@ CREATE DATABASE the_flash;
 USE the_flash;
 
 CREATE TABLE user(
-    id int(5) NOT NULL AUTO_INCREMENT,
+    id_user int(11) NOT NULL AUTO_INCREMENT,
     email varchar(100) NOT NULL,
-    username varchar(50) NOT NULL,
-    password varchar(50) NOT NULL,
-    role varchar(10) NOT NULL,
+    username varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    role varchar(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE film(
-    id int(5) NOT NULL AUTO_INCREMENT,
-    nama varchar(50) NOT NULL,
-    tahun varchar(4),
+    id_film int(11) NOT NULL AUTO_INCREMENT,
+    nama_film varchar(100) NOT NULL,
+    tahun int(4),
     sinopsis text,
+    poster varchar(300),
+    banner varchar(300),
+    link varchar(300) NOT NULL,
     PRIMARY KEY (id)
 );
